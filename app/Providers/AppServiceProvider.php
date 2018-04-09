91d864a0cdbe59593a5691063355fdb477978aa4
metal <911jason@163.com>
2018-04-09 21:41:34 +0800

    testUpload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //fix this all you have to do is edit your
+        /**
+         * Syntax error or access violation: 1071 Specified key was t
+        oo long; max key length is 1000 bytes (SQL: alter table `users` add unique
+        `users_email_unique`(`email`))
+         */
+
+        Schema::defaultStringLength(191);
+
     }
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
+    /**当我们运行迁移时，up 方法会被调用
      * Run the migrations.
      *
      * @return void
@@ -16,14 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+//            $table->string('email')->unique();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password',60);
             $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
+    /**当我们回滚迁移时，down 方法会被调用。
      * Reverse the migrations.
      *
      * @return void
