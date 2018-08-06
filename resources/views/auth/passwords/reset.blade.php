6156011da5bae567d77a82bc1b33d7c365a0882f
JasonMetal <911jason@163.com>
2018-08-06 22:56:50 +0800

    Add password resets & email configuration

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -14,7 +14,7 @@
 
                 <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
                     {{ csrf_field() }}
-
+                    <input type="hidden" name="token" value="{{$token}}">
                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                         <label for="email" class="col-md-4 control-label">邮箱地址：</label>
 
@@ -29,29 +29,29 @@
                         </div>
                     </div>
 
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label for="email" class="col-md-4 control-label">邮箱地址：</label>
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password" class="col-md-4 control-label">密码：</label>
 
                         <div class="col-md-6">
-                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                            <input id="password" type="password" class="form-control" name="password" value="{{ old('password') }}" required>
 
-                            @if ($errors->has('email'))
+                            @if ($errors->has('password'))
                                 <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                             @endif
                         </div>
                     </div>
 
-                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label for="email" class="col-md-4 control-label">邮箱地址：</label>
+                    <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                        <label for="password_confirm" class="col-md-4 control-label">确认密码：</label>
 
                         <div class="col-md-6">
-                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                            <input id="password_confirm" type="password" class="form-control" name="password_confirm" value="{{ old('password_confirmation') }}" required>
 
-                            @if ($errors->has('email'))
+                            @if ($errors->has('password_confirmation'))
                                 <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
                                     </span>
                             @endif
                         </div>
@@ -60,7 +60,7 @@
                     <div class="form-group">
                         <div class="col-md-6 col-md-offset-4">
                             <button type="submit" class="btn btn-primary">
-                                发送密码重置邮件
+                                修改密码
                             </button>
                         </div>
                     </div>
